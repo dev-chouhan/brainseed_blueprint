60a1a2a0b25e063e56ea5146681c0278ccb16db2
Dev Chouhan <devchouhan24git@gmail.com>
2023-01-31 23:00:12 +0530

    created environment

---
+++ app.js
@@ -0,0 +1,31 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+// Get requests
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+
+// Post requests
+
+
+// Serving listening on Port
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port , function () {
+    console.log("Server started Successfully on port: " + port);
+});
