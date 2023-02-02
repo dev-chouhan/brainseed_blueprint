277733586f808667560fed3aa163b868c13278dc
Sanskriti Gupta <sanskriti.gupta.005@gmail.com>
2023-02-02 23:27:49 +0530

    Add: home section

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+const ejs = require ("ejs");
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const app = express();
 
 app.set('view engine', 'ejs');
