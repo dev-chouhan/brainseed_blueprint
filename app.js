12e9072ce015e11cdd92b16fce62d14049d847a4
Dev Chouhan <68985102+dev-chouhan@users.noreply.github.com>
2023-02-02 21:21:48 +0530

    Update app.js
    added ejs module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const app = express();
 
 app.set('view engine', 'ejs');
