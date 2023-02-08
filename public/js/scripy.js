be54e73cb4295d8eaa2ad479feb9b2bb0a5799fb
Dev Chouhan <devchouhan24git@gmail.com>
2023-02-09 01:23:39 +0530

    removed unwanted code

--- public/js/scripy.js
+++ public/js/scripy.js
@@ -1,10 +1,1 @@
 console.log("Js is active");
-
-// For transversing carousel in right in every 2 sec.
-const rightSwipe = document.getElementById("nextBTN");
-function yourFunction(){
-    setInterval(function(){
-        rightSwipe.click();
-    }, 2000);
-}
-yourFunction();
