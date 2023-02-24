d975bfb2ed49b78b5deab14a1d7210bdfe07d6cb
Dev Chouhan <devchouhan24git@gmail.com>
2023-02-24 19:36:25 +0530

    added benefit of joining us page

--- public/js/script.js
+++ public/js/script.js
@@ -5,10 +5,10 @@ function animateValue(id, start, end, duration) {
     if (start === end) return;
     var range = end - start;
     var current = start;
-    var increment = end > start? 1 : -1;
+    var increment = end > start ? 1 : -1;
     var stepTime = Math.abs(Math.floor(duration / range));
     var obj = document.getElementById(id);
-    var timer = setInterval(function() {
+    var timer = setInterval(function () {
         current += increment;
         obj.innerHTML = current;
         if (current == end) {
@@ -17,8 +17,11 @@ function animateValue(id, start, end, duration) {
     }, stepTime);
 }
 
-// Calling each id elements to show increasing animation of count.
+
 //ToDo: count will start from loading of a page, we need to start it when user visits on that element
+// Calling each id elements to show increasing animation of count.
 animateValue("membersCount", 0, 500, 5000);
 animateValue("eventsCount", 0, 250, 5000);
 animateValue("opporCount", 0, 200, 5000);
+
+
