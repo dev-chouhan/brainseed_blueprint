2608b7c58750cd31fc216ee1e9d12f15a7c2a567
Dev Chouhan <devchouhan24git@gmail.com>
2023-02-25 01:12:28 +0530

    footer added

--- public/js/script.js
+++ public/js/script.js
@@ -25,3 +25,30 @@ animateValue("eventsCount", 0, 250, 5000);
 animateValue("opporCount", 0, 200, 5000);
 
 
+
+// Scroll To Top Function.
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
